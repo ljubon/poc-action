3f819dcf21678b9f643483bf7d396da5cd0fdec2
Ljubo Nikolic <ljubonikolic@gmail.com>
2022-03-12 10:14:56 +0100

    fix

--- bin/index.js
+++ bin/index.js
@@ -4,10 +4,10 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+    const nameToGreet = core.getInput('ime');
+    console.log(`Cao ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    core.setOutput("Vreme", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
